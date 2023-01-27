375a9b6878c46fc74edf22ea773be39d0382419d
julien-antigny <julien_antigny@yahoo.com>
2023-01-27 14:11:56 +0100

    add docstring

--- bobot/bobotnav.py
+++ bobot/bobotnav.py
@@ -7,36 +7,46 @@ class BobotNav(BobotWin):
         pass
 
     def reload(self):
+        """ Reload page """
         self.ctrl("f5")
 
     def go_next_browser_tab(self):
+        """ Go next browser tab """
         self.ctrl("\t")
 
     def go_prev_browser_tab(self):
+        """ Go preview browser tab """
         self.ctrl("shift", "\t")
 
     def open_browser_tab(self):
+        """ Open new browser tab """
         self.ctrl("t")
 
     def open_new_browser(self):
+        """ Open new browser """
         self.ctrl("n")
 
     def close_browser_tab(self):
+        """ Close browser tab """
         self.ctrl("w")
 
     def focus_search_bar(self):
+        """ Focus cursor on search bar """
         self.ctrl("l")
 
     def open_page(self, url: str):
+        """ Open a new page """
         self.open_browser_tab()
         self.focus_search_bar()
         self.write(url)
         self.enter()
 
-    def open_code_source_page(self):
+    def open_page_code_source(self):
+        """ Open page's code source """
         self.ctrl("u")
 
-    def save_code_source_page(backup_path: str, backup_file: str, waiting_time: float = 0.5):
+    def save_page_code_source(backup_path: str, backup_file: str, waiting_time: float = 0.5):
+        """ Save page code source """
         assert waiting_time > 0
         assert os.path.exists(backup_path)
 
